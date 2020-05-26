cab83ba35e4d58476e2b01d243d747cee29579f3
joi-joe-joy <joi.joe.joy@mail.ru>
2020-05-26 15:44:38 +0300

    module3-task2

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -3,7 +3,7 @@
 const {Router} = require(`express`);
 const myRouter = new Router();
 
-myRouter.get(`/`, (req, res) => res.send(`/my`)); // мои объявления
-myRouter.get(`/comments`, (req, res) => res.send(`/my/comments`)); // комментарии к публикациям
+myRouter.get(`/`, (req, res) => res.render(`my-tickets`)); // мои объявления
+myRouter.get(`/comments`, (req, res) => res.render(`comments`)); // комментарии к публикациям
 
 module.exports = myRouter;
